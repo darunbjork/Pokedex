be3aa7d86db81d36a1b2fe0fc785e343fa4d1fcd
Darun <darunbjork@gmail.com>
2023-07-30 00:41:31 +0200

    i added loop for to an array.

--- scripts.js
+++ scripts.js
@@ -2,6 +2,36 @@ let pokemonList = [
     {
     name: "Bulbasaur", 
     height: 7, 
-    types: ['grass', 'poison'] }
+    types: ['grass', 'poison']
+ },
+
+ {
+    name:"Charizard",
+    height: "1.7",
+    types:['fire', 'flying']
+ },
+ {
+    name:"Squirtle",
+    height: "1",
+    types:["water"]
+ }
 ];
-console.log(pokemonList);
+
+document.write(pokemonList)
+
+
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height < 8 && pokemonList[i].height > 2 ) {
+    console.log(pokemonList[i].name + " Wow that's big!");
+    }else if (pokemonList[i].height > 1){
+        console.log(pokemonList[i].name + " not big");
+    }else {
+        console.log(pokemonList[i].name + " not wow");
+    }
+    }
+
+
+
+
+
+
