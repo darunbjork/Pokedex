33226f32d584e8373afe0f7fe2d560aac93163a7
Darun <darunbjork@gmail.com>
2023-08-11 20:53:56 +0200

    using IIFE and displaying with forEach

--- scripts.js
+++ scripts.js
@@ -1,34 +1,53 @@
-let pokemonList = [
-    {
-    name: "Bulbasaur", 
-    height: 7, 
-    types: ['grass', 'poison']
- },
+// using IIFE (Immediately Invoked Function Expression) to prevent mixing variables and causing stop working 
+let pokemonRepository = (function() {
 
- {
-    name:"Charizard",
-    height: "1.7",
-    types:['fire', 'flying']
- },
- {
-    name:"Squirtle",
-    height: "1",
-    types:["water"]
- }
-];
+    let pokemonList = [
 
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height < 8 && pokemonList[i].height > 2 ) {
-    document.write(pokemonList[i].name + " Wow that's big!" + '<br>')
-    }else if (pokemonList[i].height > 1){
-        document.write(pokemonList[i].name + " not big!" + '<br>');
-    }else {
-        document.write(pokemonList[i].name + " not wow!");
+        {
+        name: "Bulbasaur", 
+        height: 7, 
+        types: ['grass', 'poison']
+     },
+    
+     {
+        name:"Charizard",
+        height: "1.7",
+        types:['fire', 'flying']
+     },
+     {
+        name:"Squirtle",
+        height: "1",
+        types:["water"]
+     },
+    ];
+// Added objects to our Pokemonlist variable  
+    function getAll () {
+        return pokemonList;
     }
+    function add (pokemon) {
+        pokemonList.push(pokemon);
+    } // added items to the pokemon
+
+    return {
+        getAll : getAll,
+        add : add
     }
 
+})();
+
+document.write(pokemonRepository.getAll());
+pokemonRepository.add({name: 'Pikachu' });
+document.write(pokemonRepository.getAll());
+
+// Using another way( forEach to iterate our objects instead of for loop) to display our objects on Dom
+pokemonList.forEach(function(item){
+    document.write(item);
+});
+    
 
 
+ 
+      
 
 
 
