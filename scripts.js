095f6c6231443f4dd04dab32594b04bc1db3896f
Darun <darunbjork@gmail.com>
2023-08-19 22:26:43 +0200

    added an IIFE and changed for lop to forEach loop and added objects to an array.

--- scripts.js
+++ scripts.js
@@ -1,60 +1,37 @@
-// using IIFE (Immediately Invoked Function Expression) to prevent mixing variables and causing stop working 
-let pokemonRepository = (function() {
+let pokemonRepository = (function () {
 
     let pokemonList = [
 
-        {
-        name: "Bulbasaur", 
-        height: 7, 
-        types: ['grass', 'poison']
-     },
+       { name: "Bulbasaur", height: 7, types: ['grass', 'poison'] },
     
-     {
-        name:"Charizard",
-        height: "1.7",
-        types:['fire', 'flying']
-     },
-     {
-        name:"Squirtle",
-        height: "1",
-        types:["water"]
-     },
-    ];
-// Added objects to our Pokemonlist variable  
-    function getAll () {
-        return pokemonList;
-    }
-    function add (pokemon) {
-        pokemonList.push(pokemon);
-    } // added items to the pokemon
-
-    return {
-        getAll : getAll,
-        add : add
-    }
-
-})();
-
-document.write(pokemonRepository.getAll());
-pokemonRepository.add({ name: "pikachu", height: 0.3, types:["electric"] });
-
-document.write(pokemonRepository.getAll());
-
-pokemonRepository.getAll().forEach(function(pokemon){
-let pokemonList = document.querySelector(".pokemon-list");
-let listItem = document.createElement =("li");
-let button = document.createElement = ("button");
-button.innerText = "placeholder";
-button.classList.add("button-class");
-listItem.appendChild(button);
-pokemonList.appendChild(listItem);
-});  
-
-
+       { name:"Charizard", height: "1.7", types:['fire', 'flying'] },
+     
+       {name:"Squirtle", height: "1", types:["water"] },
+    ]
 
  
+function getAll () {
+    return pokemonList;
+}
 
-    
+function add (pokemon) {
+    pokemonList.push(pokemon);
+}
+
+return {
+    getAll: getAll,
+    add:add
+}
+})()
+
+pokemonRepository.add({name: 'Pikachu', height: 4, types: ["rock, stone"]});
+pokemonRepository.getAll().forEach(function (pokemon){
+    let pokemonDetails = pokemon.name +  " (height: " + pokemon.height + ")<br>"
+    if (pokemon.height > 5) {
+        pokemonDetails += " Wow. That's big! <br><br>";
+    }
+    document.write(pokemonDetails)
+});
 
 
  
